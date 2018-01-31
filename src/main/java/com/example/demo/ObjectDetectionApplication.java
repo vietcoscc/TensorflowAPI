be0ad4f90f6ce8047d7d0769c646d9efcc5a2b0a
viet <vietcoscc@gmail.com>
2018-01-31 11:04:52 +0700

    commit

--- src/main/java/com/example/demo/ObjectDetectionApplication.java
+++ src/main/java/com/example/demo/ObjectDetectionApplication.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
-import nu.pattern.OpenCV;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
 public class ObjectDetectionApplication {
-    static {
-        OpenCV.loadShared();
-    }
+//    static {
+//        OpenCV.loadShared();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ObjectDetectionApplication.class, args);
