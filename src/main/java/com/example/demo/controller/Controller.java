b5d679db8d98f244abc13ea0cd49e1e595069a15
viet <vietcoscc@gmail.com>
2018-01-31 10:38:22 +0700

    Complete recognition

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -2,17 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.Utils;
 import com.example.demo.model.Classifier;
+import com.example.demo.model.Contour;
 import com.example.demo.model.RectF;
 import com.example.demo.model.TensorFlowObjectDetectionAPIModel;
 import com.google.gson.Gson;
-import org.opencv.core.*;
+import org.bytedeco.javacpp.tesseract;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.ByteArrayInputStream;
@@ -21,6 +26,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
+
+import static com.example.demo.Utils.*;
 
 @RestController
 public class Controller implements InitializingBean {
@@ -28,75 +36,88 @@ public class Controller implements InitializingBean {
     public static final String LABEL_NAME = "coco_labels_list.txt";
     public static final int INPUT_SIZE = 300;
 
-    private String resourcePath = getClass().getClassLoader().getResource("").getPath() + "/static/";
+    private String resourcePath = getClass().getClassLoader().getResource("").getPath() + "static/";
     private byte[] model;
     private Vector<String> label;
     private TensorFlowObjectDetectionAPIModel apiModel;
+    private tesseract.TessBaseAPI tessBaseAPI = new tesseract.TessBaseAPI();
 
     @PostMapping(value = "/tensorflow")
     public String tensorflow(@RequestBody String image) throws Exception {
+        image = image.replace("\"", "");
+        image = image.replace("\\n", "");
         System.out.println(image);
-
         if (image.isEmpty()) {
             return null;
         }
-        byte originalImageByte[] = org.apache.tomcat.util.codec.binary.Base64.decodeBase64(image);
+
+        byte originalImageByte[] = Base64.getDecoder().decode(image);
 
         InputStream inputStream = new ByteArrayInputStream(originalImageByte);
 
         BufferedImage original = ImageIO.read(inputStream);
         if (original == null) {
-            return "Null";
+            return null;
         }
         BufferedImage resized = Utils.resizeImage(original, original.getType());
 
         byte resizedImageByte[] = ((DataBufferByte) resized.getRaster().getDataBuffer()).getData();
-        long start = System.currentTimeMillis();
+//        long start = System.currentTimeMillis();
+
+//        if (tessBaseAPI.Init(resourcePath, "eng") != 0) {
+//            System.err.println("Could not initialize tesseract.");
+//            System.exit(1);
+//        }
+//        tessBaseAPI.SetImage(resizedImageByte,300,300,3,300*3);
+//        BytePointer r = tessBaseAPI.GetUTF8Text();
+//        System.out.println(r.getString()+"a");
 
         List<Classifier.Recognition> results = apiModel.recognizeImage(resizedImageByte);
+//        for (int i = 0; i < results.size(); i++) {
+//            Classifier.Recognition result = results.get(i);
+//            result.setLocation(Utils.resized2original(resized, original, result.getLocation()));
+//        }
+//        long end = System.currentTimeMillis() - start;
+//        RectF rectF = results.get(0).getLocation();
+//        BufferedImage cropedImage = original.getSubimage((int) rectF.left, (int) rectF.top, (int) (rectF.right - rectF.left), (int) (rectF.bottom - rectF.top));
+//
+//        Mat cropedMat = Utils.BufferedImage2Mat(cropedImage);
+//        MatOfPoint mat = processing(cropedMat);
 
-        for (int i = 0; i < results.size(); i++) {
-            Classifier.Recognition result = results.get(i);
-            result.setLocation(Utils.resized2original(resized, original, result.getLocation()));
-        }
-
-        long end = System.currentTimeMillis() - start;
-        RectF rectF = results.get(0).getLocation();
-        BufferedImage cropedImage = original.getSubimage((int) rectF.left, (int) rectF.top, (int) (rectF.right - rectF.left), (int) (rectF.bottom - rectF.top));
-        Mat cropedMat = Utils.BufferedImage2Mat(cropedImage);
-        processing(cropedMat);
-        System.out.println(end);
+//        BufferedImage processedImage = Utils.Mat2BufferedImage(mat);
 
         Gson gson = new Gson();
         return gson.toJson(results);
+
+//        System.out.println(end);
+//        return
+//        if (mat != null) {
+//            Contour contour = new Contour(rectF.left, rectF.top, mat.toList());
+//            return gson.toJson(contour);
+//        }else {
+//            return null;
+//        }
+
     }
 
-//    @PostMapping("/object")
-//    public String object(@RequestParam("person") Person person) {
-//        Gson gson = new Gson();
-//        return gson.toJson(person);
-//    }
-//
-//    @PostMapping("/file")
-//    public String file(@RequestParam(value = "file") MultipartFile file) throws Exception {
-//        if (file.getContentType().contains("video")) {
-//            byte b[] = file.getBytes();
-//            InputStream inputStream = new ByteArrayInputStream(b);
-//
-//            File file1 = new File(resourcePath + System.currentTimeMillis());
-//            FileOutputStream outputStream = new FileOutputStream(file1);
-//            outputStream.write(b);
-//            outputStream.flush();
-//            outputStream.close();
-//        }
-//        return file.getContentType();
-//    }
-//
-//    @PostMapping(value = "/")
-//    public String index(@RequestParam(value = "aa") String a) {
-//        return a;
-//    }
-//
+    @PostMapping(value = "/file")
+    public String file(@RequestPart("file") MultipartFile multipartFile) throws Exception {
+//        System.out.println(multipartFile);
+        byte b[] = multipartFile.getBytes();
+        Image image = null;
+        if (multipartFile.getContentType().contains("image")) {
+            image = ImageIO.read(new ByteArrayInputStream(b));
+        }
+        String result = image.getWidth(null) + "," + image.getHeight(null);
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(result));
+        return gson.toJson(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public String get(@PathVariable("id") String param) {
+        return param;
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -106,120 +127,5 @@ public class Controller implements InitializingBean {
         model = Files.readAllBytes(modelPath);
         label = new Vector<>(Files.readAllLines(labelPath));
         apiModel = TensorFlowObjectDetectionAPIModel.create(model, label, INPUT_SIZE);
-    }
-
-    private Mat processing(Mat input) {
-        Mat inputClone = input.clone();
-        Mat threshold = new Mat();
-        Mat gray = new Mat();
-        Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.adaptiveThreshold(gray, threshold, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
-                Imgproc.THRESH_BINARY_INV, 17, 2);
-        medianBlur(threshold);
-        multiDilate(threshold, 10);
-        multiErode(threshold, 2);
-        copyMakeBorder(threshold, 1);
-        Mat canny = new Mat();
-        Imgproc.Canny(threshold, canny, 200, 100);
-        multiDilate(canny, 5);
-        List<MatOfPoint> contours = new ArrayList<>();
-        Imgproc.findContours(canny, contours, new Mat(), Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_NONE);
-
-        MatOfPoint matOfPoint = Collections.max(contours, (l, r) -> {
-            if (Imgproc.contourArea(l) > Imgproc.contourArea(r)) {
-                return 1;
-            }
-            return 0;
-        });
-        float xCenter = 0, yCenter = 0;
-        float length = matOfPoint.toArray().length;
-        for (int i = 0; i < length; i++) {
-            xCenter += matOfPoint.toArray()[i].x;
-            yCenter += matOfPoint.toArray()[i].y;
-        }
-        xCenter /= length;
-        yCenter /= length;
-        System.out.println("CENTER : (" + xCenter + "," + yCenter + ")");
-        System.out.println("CENTER : (" + input.width() + "," + input.height() + ")");
-
-//        Imgproc.drawMarker(inputClone, new Point(xCenter, yCenter), new Scalar(255, 0, 0), Imgproc.MARKER_TILTED_CROSS, 20, 5, Imgproc.LINE_AA);
-        Imgproc.drawContours(inputClone, contours, contours.indexOf(matOfPoint), new Scalar(255, 0, 0), 3);
-
-//        float xCenter = 0, yCenter = 0;
-//        int totalPoint = 0;
-//        for (int idx = 0; idx < contours.size(); idx++) {
-//            Point[] arrPoint = contours.get(idx).toArray();
-//            for (int i = 0; i < arrPoint.length; i++) {
-//                xCenter += arrPoint[i].x;
-//                yCenter += arrPoint[i].y;
-//                System.out.print("(" + arrPoint[i].x + "," + arrPoint[i].y + ")");
-//            }
-//            totalPoint += arrPoint.length;
-//
-//            System.out.println("");
-////            if (Imgproc.contourArea(contours.get(idx)) < 10000) continue;
-//            Imgproc.drawContours(inputClone, contours, idx, new Scalar(255, 0, 0), 2);
-//        }
-//        xCenter = xCenter / totalPoint;
-//        yCenter = yCenter / totalPoint;
-//        System.out.println("CENTER : (" + xCenter + "," + yCenter + ")");
-//        Imgproc.drawMarker(inputClone, new Point(xCenter, yCenter), new Scalar(255, 0, 0), Imgproc.MARKER_TILTED_CROSS, 5, 2, Imgproc.LINE_4);
-//        medianBlur(input);
-//        Mat tmp = input.clone();
-//        medianBlur(tmp);
-        return inputClone;
-
-    }
-
-    private void copyMakeBorder(Mat mat, int strokeWidth) {
-        Point topLeft = new Point(0, 0);
-        Point topRight = new Point(mat.width(), 0);
-        Point bottomLeft = new Point(0, mat.height());
-        Point bottomRight = new Point(mat.width(), mat.height());
-
-        Core.line(mat, topLeft, topRight, new Scalar(255, 255, 255), strokeWidth);
-        Core.line(mat, topLeft, bottomLeft, new Scalar(255, 255, 255), strokeWidth);
-        Core.line(mat, topRight, bottomRight, new Scalar(255, 255, 255), strokeWidth);
-        Core.line(mat, bottomLeft, bottomRight, new Scalar(255, 255, 255), strokeWidth);
-
-//        Core.copyMakeBorder(mat, mat, strokeWidth, strokeWidth, strokeWidth, strokeWidth, Core.BORDER_CONSTANT, new Scalar(255, 255, 255));
-    }
-
-    private void blur(Mat mat) {
-        Imgproc.blur(mat, mat, new Size(10, 10));
-    }
-
-    private void medianBlur(Mat mat) {
-        Imgproc.medianBlur(mat, mat, 5);
-    }
-
-    private void gaussianBlur(Mat mat) {
-        Imgproc.GaussianBlur(mat, mat, new Size(5, 5), 2);
-    }
-
-    private void multiDilate(Mat mat, int numberLoop) {
-        for (int i = 0; i < numberLoop; i++) {
-            dilate(mat);
-        }
-    }
-
-    private void multiErode(Mat mat, int numberLoop) {
-        for (int i = 0; i < numberLoop; i++) {
-            erode(mat);
-        }
-    }
-
-    private void multiMedianBlur(Mat mat, int numberLoop) {
-        for (int i = 0; i < numberLoop; i++) {
-            medianBlur(mat);
-        }
-    }
-
-    private void dilate(Mat mat) {
-        Imgproc.dilate(mat, mat, new Mat());
-    }
-
-    private void erode(Mat mat) {
-        Imgproc.erode(mat, mat, new Mat());
     }
 }
